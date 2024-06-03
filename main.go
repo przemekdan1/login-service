05dd29d89b08867c63e9373eb1a9abc8b320e01a
przemekdan1 <jange1337@interia.pl>
2024-06-03 23:07:31 +0200

    init: setup helloworld page

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorldPage)
+	http.ListenAndServe("", nil)
+}
